99952298d09e83758ce85d71136bdabeaebe2993
Bandi gana Ram <bandiganaram@gmail.com>
2025-11-26 06:48:13 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,8 @@
-
-import { FirebaseOptions } from 'firebase/app';
-
-export const firebaseConfig: FirebaseOptions = {
-  projectId: 'studio-1321952540-2af6d',
-  appId: '1:233542470866:web:3f5e24c840d04b190d710b',
-  apiKey: 'AIzaSyAEfRmn0areHILPNMe94Y9Fi8Y5H_-vMss',
-  authDomain: 'studio-1321952540-2af6d.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '233542470866',
+export const firebaseConfig = {
+  "projectId": "studio-1321952540-2af6d",
+  "appId": "1:233542470866:web:3f5e24c840d04b190d710b",
+  "apiKey": "AIzaSyAEfRmn0areHILPNMe94Y9Fi8Y5H_-vMss",
+  "authDomain": "studio-1321952540-2af6d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "233542470866"
 };
